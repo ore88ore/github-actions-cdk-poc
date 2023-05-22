18324aff453034b7165f08254cce62cbe93e792c
Yusuke Sakai <yus_sakai@yumemi.co.jp>
2023-05-22 12:00:38 +0900

    feat: update lambda function

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ export class MyStack extends Stack {
           console.log('Hello, CDK!');
           return {
             statusCode: 200,
-            body: 'Lambda function executed successfully!',
+            body: 'Lambda function executed successfully! - update',
           };
         };
       `),
